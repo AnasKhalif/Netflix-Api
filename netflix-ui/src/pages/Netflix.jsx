24a15536f762ac1916bacd3676938be9340f6ac4
AnasKhalif <anaskhalif995@gmail.com>
2024-02-28 16:52:07 +0700

    feat : Navbar

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
 
 export default function Netflix() {
+
+  const [isScrolled, setIscrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIscrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll=null);
+  }
+
   return (
-    <div>Netflix</div>
+    <div>
+      <Navbar isScrolled={isScrolled} />
+    </div>
   )
 }
