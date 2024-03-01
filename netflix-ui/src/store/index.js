87bba69f090bfaa206c55a24f760d394cf97aa4d
AnasKhalif <anaskhalif995@gmail.com>
2024-03-01 10:12:24 +0700

    feat : Redux toolkit setup

---
+++ netflix-ui/src/store/index.js
@@ -0,0 +1,21 @@
+import {configureStore, createAsyncThunk, createSlice} from '@reduxjs/toolkit'; 
+
+
+
+const initialState = {
+    movies : [],
+    genresLoaded : false,
+    genres: [],
+};
+
+const NetflixSlice  = createSlice({
+    name : "Netflix",
+    initialState,
+    extraReducers:(builder) => {},
+});
+
+export const store = configureStore({
+    reducer: {
+        netflix:NetflixSlice.reducer,
+    },
+});
