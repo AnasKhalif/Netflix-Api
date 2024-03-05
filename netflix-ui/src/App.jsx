4167bd1ebeaea0c44b4fde0a86e7f576b3f7ab7d
AnasKhalif <anaskhalif995@gmail.com>
2024-03-05 12:51:26 +0700

    feat : Netflix TV Shows

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import Netflix from "./pages/Netflix";
 import Player from "./pages/Player";
 import Movies from "./pages/Movies";
+import TVShows from "./pages/TVShows";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="player" element={<Player />} />
         <Route exact path="/movies" element={<Movies />} />
+        <Route exact path="/tv" element={<TVShows />} />
         <Route exact path="/" element={<Netflix />} />
       </Routes>
     </BrowserRouter>
