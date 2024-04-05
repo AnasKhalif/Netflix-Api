5ad913cb586acf0157ba099ad9bf8d5a368b2ae0
AnasKhalif <anaskhalif995@gmail.com>
2024-04-05 13:45:45 +0700

    Node.js API Get All Movies

--- netflix-api/controllers/UserController.js
+++ netflix-api/controllers/UserController.js
@@ -22,3 +22,15 @@ module.exports.addToLikedMovies = async(req, res) => {
         return res.json({msg:"Error adding movie"});
     }
  };
+
+ module.exports.getLikedMovies = async(req,res) => {
+    try {
+        const {email} = req.body;
+        const user = await User.findOne({email});
+    if(user){
+        res.json({msg:"sucess", movies:user.likedMovies});
+    }else return res.json({msg: "User with givern email not found."});
+    }catch(err){
+        return res.json({msg:"Error fetching movie"});
+    }
+ }
